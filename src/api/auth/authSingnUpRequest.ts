52223b102c85c0229d97b46f84a8f34ae9d797bd
Thanhhoai230504 <nguyenthanhhoai230504@gmail.com>
2024-10-21 16:39:13 +0700

    abc

--- src/api/auth/authSingnUpRequest.ts
+++ src/api/auth/authSingnUpRequest.ts
@@ -12,11 +12,11 @@ interface User {
 }
 export const SingnUpRequest = async (payload: User) => {
     try {
-        const response: any[] = await axiosClient.get("/users");
+        const response: User[] = await axiosClient.get("/users");
         const userExists = response.find((user) => 
             user.email === payload.email || user.userName === payload.userName
         );
-        return !userExists; 
+        return !userExists; //có trả về true k có trả về false
     } catch (error) {
         return null;
     }
