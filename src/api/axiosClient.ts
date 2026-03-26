f62fbef0b1e7572886cd5692621a1a531069c688
Thanhhoai <nguyenthanhhoai230504@gmail.com>
2026-03-26 12:23:25 +0700

    a

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-   baseURL: "process.env.REACT_APP_API_URL ||http://localhost:3000/api",
+   baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
    timeout: 10000,
 });
 
